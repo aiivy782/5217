a050ad5034b6d00020f487ecc1f199fa147fde60
aiivy782 <aiivy782@gmail.com>
2023-10-28 14:40:45 +0300

    full refactor

--- app/src/main/java/com/aiivy782/pomodoro/MainActivity.kt
+++ app/src/main/java/com/aiivy782/pomodoro/MainActivity.kt
@@ -1,76 +1,98 @@
 package com.aiivy782.pomodoro
 
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.view.View
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import java.util.concurrent.TimeUnit
 import com.aiivy782.pomodoro.databinding.ActivityMainBinding
+import java.util.Date
+import java.util.Timer
+import java.util.TimerTask
 
-    class MainActivity : AppCompatActivity() {
-        private lateinit var binding: ActivityMainBinding
-        private var isTimerRunning = false
-        var timer: CountDownTimer? = null
-        val workTimeMillis = TimeUnit.MINUTES.toMillis(52)
-        val breakTimeMillis = TimeUnit.MINUTES.toMillis(17)
-        override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var dataHelper: DataHelper
 
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+    private val timer = Timer()
 
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            super.onCreate(savedInstanceState)
-            setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        dataHelper = DataHelper(applicationContext)
 
-            binding.startPauseButton.setOnClickListener {
-                if (isTimerRunning) {
-                    pauseTimer()
-                } else {
-                    startTimer()
-                }
+        binding.startPauseButton.setOnClickListener{ startStopAction() }
+        binding.resetButton.setOnClickListener{ resetAction() }
+
+        if (dataHelper.timerCounting()) {
+            startTimer()
+        }
+        else {
+            stopTimer()
+            if (dataHelper.startTime() != null && dataHelper.stopTime() != null) {
+                val time = Date().time - calcRestartTime().time
+                binding.timerTV.text = timeStringFromLong(time)
             }
+        }
 
-        binding.resetButton.setOnClickListener {
-            resetTimer()
+        timer.scheduleAtFixedRate(TimeTask(), 0, 500)
+
+    }
+
+    private inner class TimeTask: TimerTask() {
+        override fun run() {
+            if (dataHelper.timerCounting()) {
+                val time = Date().time - dataHelper.startTime()!!.time
+                binding.timerTV.text = timeStringFromLong(time)
+            }
         }
     }
 
+    private fun startStopAction() {
+        if (dataHelper.timerCounting()) {
+            dataHelper.setStopTime(Date())
+            stopTimer()
+        }
+        else {
+            if (dataHelper.stopTime() != null) {
+                dataHelper.setStartTime(calcRestartTime())
+                dataHelper.setStopTime(null)
+            }
+            else {
+                dataHelper.setStartTime(Date())
+            }
+            startTimer()
+        }
+    }
+
+    private fun calcRestartTime(): Date {
+        val diff = dataHelper.startTime()!!.time - dataHelper.stopTime()!!.time
+        return Date(System.currentTimeMillis() + diff)
+    }
+
+    private fun stopTimer() {
+        dataHelper.setTimerCounting(false)
+        binding.startPauseButton.text = getString(R.string.start)
+    }
+
     private fun startTimer() {
-        isTimerRunning = true
-        binding.startPauseButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.pause, 0, 0, 0) // Иконка "Пауза"
-        binding.resetButton.visibility = View.VISIBLE
-        binding.startPauseButton.backgroundTintList = getColorStateList(R.color.material_you_accent) // Изменение цвета под Material You
-
-        var timer = object : CountDownTimer(workTimeMillis, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                val minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)
-                val seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60
-                binding.timerTextView.text = String.format("%02d:%02d", minutes, seconds)
-            }
-
-            override fun onFinish() {
-                // Здесь можно добавить обработку смены режима (работа/перерыв)
-                // и включить таймер для следующего интервала
-            }
-        }.start()
+        dataHelper.setTimerCounting(true)
+        binding.startPauseButton.text = getString(R.string.pause)
     }
 
-    private fun pauseTimer() {
-        isTimerRunning = false
-        binding.startPauseButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.play_arrow, 0, 0, 0) // Иконка "Старт"
-        binding.resetButton.visibility = View.VISIBLE
-        binding.startPauseButton.backgroundTintList = getColorStateList(R.color.material_you_accent) // Изменение цвета под Material You
-
-        timer?.cancel()
+    private fun resetAction() {
+        dataHelper.setStopTime(null)
+        dataHelper.setStartTime(null)
+        stopTimer()
+        binding.timerTV.text = timeStringFromLong(0)
     }
 
-    private fun resetTimer() {
-        isTimerRunning = false
-        binding.startPauseButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.stop, 0, 0, 0) // Иконка "Старт"
-        binding.resetButton.visibility = View.GONE
-        binding.startPauseButton.backgroundTintList = getColorStateList(R.color.material_you_accent) // Изменение цвета под Material You
+    private fun timeStringFromLong(ms: Long): String {
+        val seconds = (ms / 1000) % 60
+        val minutes = (ms / (1000 * 60) % 60)
+        return makeTimeString(minutes, seconds)
+    }
 
-        timer?.cancel()
-        binding.timerTextView.text = "00:00"
+    private fun makeTimeString(minutes: Long, seconds: Long): String {
+        return String.format("%02d:%02d", minutes, seconds)
     }
 }
